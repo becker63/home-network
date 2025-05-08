8a87fb430a6471b0fa2bb677e6124a3a7625c05a
Taylor Timmothy Johnson <becker@M2-Air.local>
2025-05-07 21:24:27 -0400

    more explicit about hardware

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -1,11 +1,7 @@
 from invoke.collection import Collection
-from talos_home_main import ns as bootstrap_ns
 from sopsy_tasks import sopsy_tasks
 
 ns = Collection()
-
-# Add bootstrap tasks under `bootstrap.*`
-ns.add_collection(bootstrap_ns, name="talos_home_main")
 
 # Add sopsy tasks under `sopsy.*`
 ns.add_collection(Collection.from_module(sopsy_tasks), name="sopsy")
